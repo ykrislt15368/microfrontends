ee18cec0a15201af97a7110ff6947fbf650c4fcc
Yelleti Krishna <krishna.yelleti@publicissapient.com>
2023-02-20 17:14:06 +0530

    packages updat

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi checking CI/CD!!</h1>
+      <h1>Hi checking CI/CD!!!</h1>
       <hr />
       <MarketingApp />
     </div>
