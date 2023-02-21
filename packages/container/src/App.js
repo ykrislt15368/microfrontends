96dfa91d6c9098c71263008a64a6e33c301f0c9a
Yelleti Krishna <krishna.yelleti@publicissapient.com>
2023-02-21 11:23:45 +0530

    Added Header!

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+
 
 export default () => {
   return (
+    <BrowserRouter>
     <div>
-      <h1>Hi checking CI/CD!!</h1>
-      <hr />
+      <Header/>
       <MarketingApp />
     </div>
+    </BrowserRouter>
   );
 };
