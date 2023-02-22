b4c1c755151e31af364b49fe6a03fbb1b1607fa5
Yelleti Krishna <krishna.yelleti@publicissapient.com>
2023-02-22 10:26:03 +0530

    Added dashboard frontend

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,13 +5,12 @@ import {
   createGenerateClassName,
 } from '@material-ui/core/styles';
 import { createBrowserHistory } from 'history';
-
 import Progress from './components/Progress';
 import Header from './components/Header';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
-//const DashboardLazy = lazy(() => import('./components/DashboardApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
@@ -41,10 +40,10 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
-              {/* <Route path="/dashboard">
+              <Route path="/dashboard">
                 {!isSignedIn && <Redirect to="/" />}
                 <DashboardLazy />
-              </Route> */}
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
